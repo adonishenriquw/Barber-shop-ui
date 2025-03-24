60eabe93113859429885585503d2bcba447ccaf2
adonishenriquw <adonis.henrique21@gmail.com>
2025-03-24 15:48:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { CardHeaderComponent } from './commons/components/card-header/card-header.component';
+import { filter, map, Subscription } from 'rxjs';
+import { MenuBarComponent } from './commons/components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardHeaderComponent, MenuBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'barber-shop-ui';
+
+  private routeSubscription?: Subscription;
+
+  constructor(
+    private readonly router: Router,
+    private readonly ActivatedRoute: ActivatedRoute
+  ) {}
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.getRouterTitle(this.ActivatedRoute))
+      )
+      .subscribe((title) => (this.title = title));
+  }
+
+  private getRouterTitle(route: ActivatedRoute): string {
+    let child = route;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    return child.snapshot.data['title'] || 'default title';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { EditClientComponent } from './clients/edit-client/edit-client.component';
+import { NewClientComponent } from './clients/new-client/new-client.component';
+import { ListClientsComponent } from './clients/list-clients/list-clients.component';
+import { ScheduleCalendarComponent } from './schedules/components/schedule-calendar/schedule-calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'clients/editclient/:id',
+    component: EditClientComponent,
+    data: { title: 'Atualizar cliente' },
+  },
+  {
+    path: 'clients/new-client',
+    component: NewClientComponent,
+    data: { title: 'Cadastrar cliente' },
+  },
+  {
+    path: 'clients/new-list',
+    component: ListClientsComponent,
+    data: { title: 'Cliente Cadastrados' },
+  },
+  {
+    path: 'schedules/month',
+    component: ScheduleCalendarComponent,
+    data: { title: 'Agendamentos' },
+  },
+  { path: '**', redirectTo: 'schedules/month' },
+];
